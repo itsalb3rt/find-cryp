f2b969785f442ee117952b1f2f383b99218257ad
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2021-11-02 13:57:41 -0400

    feat
    * Use CoinMarketCap API

--- src/app.js
+++ src/app.js
@@ -9,30 +9,52 @@ const port = 3000
 app.use(express.static(__dirname + '/public'));
 
 app.get('/api/crypto', async (req, res) => {
-
   const minPrice = req.query.minPrice
   const maxPrice = req.query.maxPrice
   const maxSupply = req.query.maxSupply
   const dateAdded = req.query.dateAdded
   const networks = req.query.network
+  // pagination
+  const limit = Number(req.query.limit) || 10
+  const offset = Number(req.query.offset) || 1
+
+  let qs = `?start=${offset}&limit=${limit}&convert=USD&price_min=${minPrice}&price_max=${maxPrice}&circulating_supply_max=${maxSupply}`
+
   
-  let qs = `?start=1&limit=5000&convert=USD&price_min=${minPrice}&price_max=${maxPrice}&circulating_supply_max=${maxSupply}`
   try {
     let response = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest' + qs, {
-        headers: { 'X-CMC_PRO_API_KEY': process.env.COINMARKETCAP_API_KEY }
+      headers: {
+        'X-CMC_PRO_API_KEY': `${process.env.API_KEY}`
+      }
     });
+
+    // Parse data from API
+
+    let coins = response.data.data;
+    coins.forEach(coin => {
+      coin.marketCapUsd = coin.marketCapUsd ? parseFloat(coin.marketCapUsd) : 0
+      coin.priceUsd = parseFloat(coin.priceUsd)
+      coin.volumeUsd24Hr = parseFloat(coin.volumeUsd24Hr)
+      coin.maxSupply = coin.maxSupply ? parseFloat(coin.maxSupply) : 0
+      coin.supply = coin.supply ? parseFloat(coin.supply) : 0
+      coin.changePercent24Hr = parseFloat(coin.changePercent24Hr)
+    })
 
     if (networks) {
       // filter response tags.includes include any networks
       response.data.data = response.data.data.filter(coin => coin.tags.includes(networks))
     }
 
-    // filter response dateAdded
     if (dateAdded) {
       response.data.data = response.data.data.filter(coin => coin.date_added > dateAdded)
     }
 
-    res.json(response.data.data)
+    res.json({
+      data: coins,
+      hasMore: coins.length === limit,
+      offset: offset === 0 ? limit + 1 : limit + offset
+    })
+    
   } catch (error) {
     console.log(error);
   }
